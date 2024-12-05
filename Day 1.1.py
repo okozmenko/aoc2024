92f3a89ed34315ba14feadb90b389db23adb47ff
okozmenko <kozmenko.oleksii@gmail.com>
2024-12-05 16:03:52 +0100

    Update Day 1.1.py
    the solution

--- Day 1.1.py
+++ Day 1.1.py
@@ -1,2 +1,18 @@
 with open("data.txt") as f:
     lines = f.readlines()
+
+a1 = []
+a2 = []
+for line in lines:
+    e1, e2 = ([int(x.strip()) for x in (line.split("   "))])
+    a1.append(e1)
+    a2.append(e2)
+    
+a1.sort()
+a2.sort()
+
+dist = 0
+for index, item in enumerate(a1):
+    dist += abs(item-a2[index])
+
+print(dist)
